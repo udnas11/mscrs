ba5456b0ddd3e337d31dc7336a2b07b3edcf817e
AlexRoHome <cdeadlys@gmail.com>
2018-02-23 22:05:48 +0200

    water shader WIP

---
+++ mscrs_UnityProject/Assets/Scripts/Water.cs
@@ -0,0 +1,46 @@
+﻿// Made by Alexandru Romanciuc <sanromanciuc@gmail.com>
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityRandom = UnityEngine.Random;
+
+public class Water : MonoBehaviour
+{
+    #region public serialised vars
+    [SerializeField]
+    Transform _reflectPoint;
+    #endregion
+
+
+    #region private protected vars
+    Material _material;
+    #endregion
+
+
+    #region pub methods
+    #endregion
+
+
+    #region private protected methods
+    #endregion
+
+
+    #region events
+    #endregion
+
+
+    #region mono events
+    private void Awake()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        _material.SetVector("_ReflectPoint", Camera.main.WorldToViewportPoint(_reflectPoint.position));
+        //_material.SetVector("_ReflectPoint", _reflectPoint.position - transform.position);
+    }
+    #endregion
+}
