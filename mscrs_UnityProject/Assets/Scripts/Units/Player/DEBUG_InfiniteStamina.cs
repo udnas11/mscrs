63db3f80712513be379b493b19db32fbfe28d306
AlexRoHome <cdeadlys@gmail.com>
2018-02-27 15:59:23 +0200

    set up stamina for all animations && infinite stamina for player on keypress u

---
+++ mscrs_UnityProject/Assets/Scripts/Units/Player/DEBUG_InfiniteStamina.cs
@@ -0,0 +1,29 @@
+﻿// Made by Alexandru Romanciuc <sanromanciuc@gmail.com>
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityRandom = UnityEngine.Random;
+
+public class DEBUG_InfiniteStamina : MonoBehaviour
+{
+
+    bool _active;
+    PlayerController _player;
+
+    private void Awake()
+    {
+        _player = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            _active = !_active;
+
+        if (_active)
+            _player.DecreaseStamina(-100f);
+    }
+
+}
